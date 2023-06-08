e09b7052eb302ee9b6a6362507e4f3baf077df69
xuanwei zhang <xuanwei.zhang@gatech.edu>
2023-06-08 16:16:59 +0800

    feat(catalog): 新增types mod以及catalog mod. - catalog 存储了库表列之间的关系结构 - types   类型系统表示了数据库字段的类型

---
+++ src/types/numeric_types.rs
@@ -0,0 +1,45 @@
+use crate::types::{DataType, DataTypeEnum, DataTypeRef};
+use std::any::Any;
+use std::sync::Arc;
+
+/// Int32Type is a 32-bit integer type
+pub(crate) struct Int32Type {
+    nullable: bool,
+}
+
+impl DataType for Int32Type {
+    fn is_nullable(&self) -> bool {
+        self.nullable
+    }
+
+    fn get_type(&self) -> DataTypeEnum {
+        DataTypeEnum::Int32
+    }
+
+    fn get_data_len(&self) -> u32 {
+        4
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Int32Type {
+    pub(crate) fn new(nullable: bool) -> DataTypeRef {
+        Arc::new(Self { nullable })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_int32() {
+        let int32_type = Int32Type::new(false);
+        assert_eq!(int32_type.as_ref().is_nullable(), false);
+        assert_eq!(int32_type.as_ref().get_data_len(), 4);
+    }
+}
